adf4b240fdb1866ca050d147343a7b3d2fbdb6cb
twinh <twinhuang@qq.com>
2020-07-08 16:18:57 +0800

    feat: add monorepo cli option, use to disable monorepo mode

--- cli.js
+++ cli.js
@@ -25,6 +25,7 @@ Usage:
     .option('p', {alias: 'plugins', describe: 'Plugins', ...stringList, group: 'Options'})
     .option('e', {alias: 'extends', describe: 'Shareable configurations', ...stringList, group: 'Options'})
     .option('ci', {describe: 'Toggle CI verifications', type: 'boolean', group: 'Options'})
+    .option('m', {alias: 'monorepo', describe: 'Toggle monorepo mode', type: 'boolean', group: 'Options'})
     .option('verify-conditions', {...stringList, group: 'Plugins'})
     .option('analyze-commits', {type: 'string', group: 'Plugins'})
     .option('verify-release', {...stringList, group: 'Plugins'})
